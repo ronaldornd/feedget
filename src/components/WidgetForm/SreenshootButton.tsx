da3b7bd219cfed68f225adfc6a51c3bb9514c4cc
Ronaldo Barbosa <ronaldo.s.barbosa@outlook.com>
2022-06-18 12:45:23 -0300

    Add my Styles

--- src/components/WidgetForm/SreenshootButton.tsx
+++ src/components/WidgetForm/SreenshootButton.tsx
@@ -2,40 +2,40 @@ import html2canvas from "html2canvas";
 import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
 import { Loading } from "./Loading";
-interface ScreenshootButtonProps{
+interface ScreenshootButtonProps {
     screenshoot: string | null;
-    onScreenshootTook: (screenshoot:string | null) => void;
+    onScreenshootTook: (screenshoot: string | null) => void;
 }
-export function ScreenshootButton({screenshoot, onScreenshootTook}: ScreenshootButtonProps) {
+export function ScreenshootButton({ screenshoot, onScreenshootTook }: ScreenshootButtonProps) {
     const [isTakingScreenshoot, setIsTakingSreenshoot] = useState(false);
-    async function handleTakeScreenshoot(){
+    async function handleTakeScreenshoot() {
         setIsTakingSreenshoot(true);
         const canvas = await html2canvas(document.querySelector('html')!);
         const base64image = canvas.toDataURL('image/png');
         onScreenshootTook(base64image);
         setIsTakingSreenshoot(false);
     }
-    if(screenshoot){
-        return(
+    if (screenshoot) {
+        return (
             <button
-            type="button"
-            className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
-            onClick={() => onScreenshootTook(null)}
-            style={{
-                backgroundImage: `url(${screenshoot})`,
-                backgroundPosition: `right bottom`,
-                backgroundSize: 200
-            }}>
-                <Trash weight="fill"/>
+                type="button"
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                onClick={() => onScreenshootTook(null)}
+                style={{
+                    backgroundImage: `url(${screenshoot})`,
+                    backgroundPosition: `right bottom`,
+                    backgroundSize: 200
+                }}>
+                <Trash weight="fill" />
 
             </button>
         )
     }
-    return(
-    <button
-        type="button"
-        onClick={handleTakeScreenshoot}
-        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
-            {isTakingScreenshoot ? <Loading/> : <Camera className="w-6 h-6 text-zinc-100" />}
-    </button>)
+    return (
+        <button
+            type="button"
+            onClick={handleTakeScreenshoot}
+            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
+            {isTakingScreenshoot ? <Loading /> : <Camera className="w-6 h-6 text-zinc-100" />}
+        </button>)
 }
--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -4,28 +4,28 @@ import { CloseButton } from "../../CloseButton";
 interface FeedbackTypesProps {
     onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
-export function FeedbackTypeStep({onFeedbackTypeChanged}:FeedbackTypesProps){
-    return(
+export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypesProps) {
+    return (
         <>
-        <header>
+            <header>
                 <span className="text-xl leading-6">Deixe seu Feedback!</span>
-                <CloseButton/>
+                <CloseButton />
             </header>
-        <div className="flex py-7 gap-2 w-full">
-                    { Object.entries(feedbackTypes).map(([key, value]) => {
-                        return(
-                            <button
-                                key={key}
-                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-4 border-transparent hover:border-violet-500 focus:border-violet-500 focus:outline-none"
-                                type="button"
-                                onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-                            >
-                                <img className="w-12 h-12" src={value.image.source} alt={value.image.alt}/>
-                                <span>{value.title}</span>
-                            </button>
-                        );
-                    })}
-        </div>
+            <div className="flex py-7 gap-2 w-full">
+                {Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            key={key}
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-4 border-transparent hover:border-red-500 focus:border-red-500 focus:outline-none"
+                            type="button"
+                            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+                        >
+                            <img className="w-12 h-12" src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
         </>
     )
 }
