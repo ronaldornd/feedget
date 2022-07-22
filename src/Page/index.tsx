0ab08b067703f3766b87dff8464d02a53166740c
Ronaldo Barbosa <60077368+ronaldornd@users.noreply.github.com>
2022-07-22 13:05:24 -0300

    fix

--- src/Page/index.tsx
+++ src/Page/index.tsx
@@ -25,7 +25,7 @@ export default function page() {
                     setMessage('Obrigado pelo interesse no meu perfil 🥰')
                     break;
                 case 5:
-                    setMessage('Estou testando uma nova ferramenta')
+                    setMessage('Estou testando uma nova ferramenta 🚧')
                     break;
                 case 7:
                     setMessage('E gostaria muito que você pudesse me ajudar!')
@@ -35,8 +35,10 @@ export default function page() {
                     break;
                 case 13:
                     setMessage('Fica tranquilo que é tudo seguro, só não tive 💲 para colocar na PlayStore ')
-                    clearInterval(timer)
+                    break;
+                case 17:
                     setAlerting(false)
+                    clearInterval(timer)
                     break;
             }
         }, 2000);
@@ -46,35 +48,41 @@ export default function page() {
 
 
     return (
-        <div className="flex-col  h-screen items-center self-center w-screen p-20 content-center">
-            {alerting ?
-                (
-                    <div className="flex flex-col border-4 border-black shadow-2xl shadow-green-800 self-center rounded-xl h-full w-full justify-center  p-10 bg-green-500">
-                        <AnimatedText
-                            type="chars" // animate words or chars
+        <>
+            <div className="flex-col p-10 w-screen items-center justify-center self-center ontent-center">
+                {alerting ?
+                    (
+                        <div className="flex-col mt-10 justify-center">
+                            <div className="flex py-60  flex-col h-96 border-4 border-black shadow-2xl shadow-green-800 text-center rounded-xl  justify-center  bg-green-500">
+                                <AnimatedText
+                                    type="chars" // animate words or chars
 
-                            animationType="lights"
-                            interval={0.1}
-                            duration={0.09}
-                            tag="h1"
-                            className="animated-paragraph text-center text-3xl text-black font-semibold"
-                            includeWhiteSpaces
-                            threshold={1}
-                            rootMargin="20%">{message}</AnimatedText>
-                    </div>
-                ) :
-                <div className="flex flex-col items-center justify-center h-screem w-sreen">
-                    <h1 className="text-4xl flex justify-self-center font-serif font-bold self-center mb-4">Compartilhe!</h1>
-                    <img src={qrImage} className='flex w-64 mb-0 self-center' />
-                    <a href="https://expo.dev/artifacts/eas/7HTyXvzvnEbV78YNm6XUpo.apk">
-                        <div className="flex flex-row self-center gap-2 p-3 mt-10 rounded-2xl align-text-middle justify-center w-100 bg-green-500">
+                                    animationType="lights"
+                                    interval={0.1}
+                                    duration={0.09}
+                                    tag="h1"
+                                    className="animated-paragraph text-center p-10 text-3xl text-black font-semibold"
+                                    includeWhiteSpaces
+                                    threshold={1}
+                                    rootMargin="20%">{message}</AnimatedText>
+                            </div>
 
-                            <img src={downloadImage} className='flex w-10 self-center h-10' />
-                            <h1 className="flex self-center text-4xl p-2 text-black font-mono font-bold">Download</h1>
                         </div>
-                    </a>
-                </div>}
+                    ) :
+                    <div className="flex flex-col gap-12 items-center justify-center h-screem w-sreen">
+                        <h1 className="text-5xl flex justify-self-center font-serif font-bold self-center">Compartilhe!</h1>
+                        <img src={qrImage} className='flex h-64 self-center' />
+                        <a className="h-full " href="https://expo.dev/artifacts/eas/7HTyXvzvnEbV78YNm6XUpo.apk">
+                            <div className="flex flex-row self-center gap-2 p-3  rounded-2xl align-text-middle justify-center w-full bg-green-500">
 
-        </div>
+                                <img src={downloadImage} className='flex w-10 self-center h-10' />
+                                <h1 className="flex self-center text-4xl p-2 text-black font-mono font-bold">Download</h1>
+                            </div>
+                        </a>
+
+                    </div>
+                }
+            </div>
+        </>
     )
 }
